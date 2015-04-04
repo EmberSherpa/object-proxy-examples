240f197b4d29ba2dcd8e3a62b2c465b02b16a67a
Taras Mankovski <tarasm@gmail.com>
2015-04-04 19:22:55 -0400

    Added comments to controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  /**
+   * Computed Property that evaluates to array of ObjectProxy objects that proxy original model.
+   * The proxied object has isActive property that's used in the template to bind checkboxes.
+   * This allows us to select checkboxes without modifying the proxied content
+   * @see http://emberjs.com/api/classes/Ember.ObjectProxy.html
+   */
   proxiedModels: Ember.computed.map('model', function(object){
     return Ember.ObjectProxy.create({
       content: object,
       isActive: false
     });
   }),
+  /**
+   * Computed Property that evaluates to array of object proxy instances that have isActive === true
+   */
   proxiedSelected: Ember.computed.filterBy('proxiedModels', 'isActive', true),
+  /**
+   * Computed Property that evaluates to array of object proxy instances that have isActive === false
+   */
   proxiedRest: Ember.computed.filterBy('proxiedModels', 'isActive', false),
+  /**
+   * Computed Property that evaluates to an array of selected objects (not proxies)
+   */
   selected: Ember.computed.map('proxiedSelected', function(proxied){
     return proxied.content;
   })
